b58bb231e32dbe3f6dadf475941bd024ef053b9b
Calvin-Wijaya <67195371+Calvin-Wijaya@users.noreply.github.com>
2022-07-07 14:58:39 +0700

    initial

---
+++ src/app/view/user/user.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
+import { MaterialModule } from 'src/app/material/material.module';
+
+@NgModule({
+  declarations: [UserComponent],
+  imports: [CommonModule, UserRoutingModule, MaterialModule],
+})
+export class UserModule {}
