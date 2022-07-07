7dde07dd9630951b5d31d8d9eb37fcd1dfc57949
Calvin-Wijaya <67195371+Calvin-Wijaya@users.noreply.github.com>
2022-07-08 01:48:41 +0700

    update navigation

---
+++ src/app/view/user/components/search-brand/search-brand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-brand',
+  templateUrl: './search-brand.component.html',
+  styleUrls: ['./search-brand.component.scss']
+})
+export class SearchBrandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/view/user/user.module.ts
+++ src/app/view/user/user.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { MaterialModule } from 'src/app/material/material.module';
+import { NavInfoComponent } from './components/nav-info/nav-info.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeadlineComponent } from './components/headline/headline.component';
+import { SearchBrandComponent } from './components/search-brand/search-brand.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, NavInfoComponent, NavigationComponent, HeadlineComponent, SearchBrandComponent],
   imports: [CommonModule, UserRoutingModule, MaterialModule],
 })
 export class UserModule {}
